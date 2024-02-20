92b1befb3407298c123f23891b57901e6cdf7796
zlzzlzlz <zlz20121101@163.com>
2024-02-20 23:33:05 +0800

    Site updated: 2024-02-20 23:33:05

--- js/duration.js
+++ js/duration.js
@@ -1,6 +1,6 @@
 !(function() {
     /** 计时起始时间，自行修改 **/
-    var start = new Date("2023/02/20 00:00:00");
+    var start = new Date("2023/02/19 23:32:00");
   
     function update() {
       var now = new Date();
